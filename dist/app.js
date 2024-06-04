44d713e58320a8679ef655d00871ac485256838a
shivangi-gupta-01 <110471751+shivangi-gupta-01@users.noreply.github.com>
2024-06-04 12:04:03 +0530

    error fixed

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,19 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const prjInput = new ProjectInput();
+//# sourceMappingURL=app.js.map
